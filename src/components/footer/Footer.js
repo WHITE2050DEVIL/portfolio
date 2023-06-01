6724494ba538dbe744783e02023f7946295b8a35
Soheel Gadad <108049316+SoheelGadad@users.noreply.github.com>
2023-06-01 17:28:02 +0530

    update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,8 +14,8 @@ export default function Footer() {
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+          <a href="https://github.com/WHITE2050DEVIL/portfolio">
+            _____
           </a>
         </p>
       </div>
